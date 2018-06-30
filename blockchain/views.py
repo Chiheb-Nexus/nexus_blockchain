7dd7d0dfdd3529211bf9967bcc15c55e56b46ae0
chiheb-nexus <chihebnexus@gmail.com>
2018-06-30 14:53:14 +0100

    update views

--- blockchain/views.py
+++ blockchain/views.py
@@ -1,6 +1,5 @@
 #
-# https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
-# https://medium.com/crypto-currently/lets-make-the-tiniest-blockchain-bigger-ac360a328f4d
+# Main views
 #
 
 import re
