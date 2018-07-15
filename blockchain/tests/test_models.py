2c1ca2d4f19644ec1eb636342946f1f57ff5243b
Chiheb NeXus <chihebnexus@gmail.com>
2018-07-15 15:21:40 +0100

    Add more model tests

--- blockchain/tests/test_models.py
+++ blockchain/tests/test_models.py
@@ -7,6 +7,7 @@ import os
 from datetime import datetime
 from web3.auto import w3
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from blockchain.models import (
     BlockStructureDB,
     TransactionDB,
@@ -65,6 +66,48 @@ class TestBlockStructureDB(TestCase):
         self.assertIsNone(block.merkle)
 
 
+class TestAddress(TestCase):
+    def setUp(self):
+        self.address1 = '0x4c6AfD202389c720e9C2550B6022dF26321A94FC'
+        self.address2 = self.address1.lower()
+        self.address3 = '0x4c6AfD202389c720e9C2550B6022dF26321A94F'
+
+    def test_address(self):
+        '''Test if the given address is a valid Ethereum address'''
+        # Pass
+        try:
+            addr1 = Address()
+            addr1.address = self.address1
+            addr1.balance = 10
+            addr1.full_clean()
+            addr1.save()
+        except ValidationError as e:
+            # We'll not go to this section
+            self.assertIsNone(e)
+
+        # Pass
+        try:
+            addr2 = Address()
+            addr2.address = self.address2
+            addr2.balance = 10
+            addr2.full_clean()
+            addr2.save()
+        except ValidationError as e:
+            # We'll not go to this section
+            self.assertIsNone(e)
+
+        # Fail
+        try:
+            addr3 = Address()
+            addr3.address = self.address3
+            addr3.balance = 10
+            addr3.full_clean()
+            addr3.save()
+        except ValidationError as e:
+            # Silence the error
+            self.assertIsNotNone(e)
+
+
 class TestTransactionDB(TestCase):
     def generate_account(self):
         '''Generate random private & public addresses'''
@@ -84,6 +127,29 @@ class TestTransactionDB(TestCase):
             timestamp=datetime.now().timestamp(),
             data="tx sent!"
         )
+        # more tx with a valid block
+        block = BlockStructureDB.objects.create(
+            height=1,
+            timestamp=datetime.now().timestamp(),
+            data='Block 1',
+            block_hash='0x00000000FF',
+        )
+        tx2 = TransactionDB.objects.create(
+            sender=sender,
+            reciever=reciever,
+            amount=3,
+            timestamp=datetime.now().timestamp(),
+            data="tx sent!",
+            block=block
+        )
+        tx3 = TransactionDB.objects.create(
+            sender=sender,
+            reciever=reciever,
+            amount=1,
+            timestamp=datetime.now().timestamp(),
+            data="tx sent!",
+            block=block
+        )
 
     def test_valid_tx(self):
         '''Check if Transaction is well created'''
@@ -92,6 +158,14 @@ class TestTransactionDB(TestCase):
         self.assertEqual(tx.reciever.address, self.address2)
         self.assertIsNotNone(tx.timestamp)
         self.assertEqual(tx.data, 'tx sent!')
-        self.assertIsNone(tx.block)
+        self.assertIsNotNone(tx.block)
         self.assertEqual(tx.confirmation, 0)
         self.assertEqual(tx.fees, 0)
+
+    def test_valid_tx_block(self):
+        '''Check if a transaction can be added with a valid block'''
+        tx = TransactionDB.objects.first()
+        # Save the block to generate a merkle root
+        tx.block.save()
+        self.assertEqual(tx.sender.address, self.address1)
+        self.assertIsNotNone(tx.block.merkle)
