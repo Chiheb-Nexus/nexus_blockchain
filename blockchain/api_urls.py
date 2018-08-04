b215671807f744e5d4433a552ae2bc641bfe396d
chiheb-nexus <chihebnexus@gmail.com>
2018-08-04 19:31:44 +0100

    update genesis url

--- blockchain/api_urls.py
+++ blockchain/api_urls.py
@@ -1,13 +1,13 @@
 #
 # API URLs
 #
+"""API URLs"""
 
 from django.urls import path, include, re_path
 from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
 from blockchain import api_views
 
-
+# pylint: disable=C0103
 router = routers.DefaultRouter()
 router.register(
     r'get-raw-transaction',
